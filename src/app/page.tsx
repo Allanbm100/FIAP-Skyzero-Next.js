0836dba54875b4b50e6b8e5a5640461c04c22f81
CaioLiang <caioliang2@gmail.com>
2024-11-22 22:02:07 -0300

    adicionando section 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
+import { FaCalculator } from "react-icons/fa";
+import { IoIosSave } from "react-icons/io";
+import { IoPieChartSharp } from "react-icons/io5";
+import { FaGithub } from "react-icons/fa6";
 import backgroundImage from "@/assets/images/section1-wpp.jpg"
 import Image from "next/image"
 
@@ -26,7 +30,23 @@ export default function Home() {
           </p>
         </div>
       </section>
-
+      <section className="flex flex-col items-center gap-5 py-20">
+        <h1 className="text-2xl sm:text-3xl font-semibold text-[#079b11] mb-5">Nossos Serviços</h1>
+        <ul className="flex flex-col xl:flex-row gap-4 text-center">
+          <li className="flex flex-col items-center gap-2 py-2 w-40 sm:w-80 lg:w-96 xl:w-80 2xl:w-96 border-2 rounded-xl border-[#079b11]">
+            <FaCalculator size={50} color="079b11" />
+            <p className="text-gray-500">Cálculo de Co2 disperso por voo</p>
+          </li>
+          <li className="flex flex-col items-center gap-2 py-2 w-40 sm:w-80 lg:w-96 xl:w-80 2xl:w-96 border-2 rounded-xl border-[#079b11]">
+            <IoIosSave size={50} color="079b11" />
+            <p className="text-gray-500">Registros de voos em seu perfil</p>
+          </li>
+          <li className="flex flex-col items-center gap-2 py-2 w-40 sm:w-80 lg:w-96 xl:w-80 2xl:w-96 border-2 rounded-xl border-[#079b11]">
+            <IoPieChartSharp size={50} color="079b11" />
+            <p className="text-gray-500">Gráficos de dispersão e diminuições</p>
+          </li>
+        </ul>
+      </section>
     </main>
   )
 }
