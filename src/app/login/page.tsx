9b1e3edd1b392f36a421c81fc96b273d2de5b02f
allan.bm.100@gmail.com <caioliang2@gmail.com>
2024-11-22 22:42:06 -0300

    adicionando usestates necessarios

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,4 +1,29 @@
+"use client"
+
+import { PiKeyReturnFill } from "react-icons/pi";
+import Link from "next/link";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { useUserContext } from "@/context/UserContext";
+
+
 export default function Login() {
+    const router = useRouter();
+
+    const { setIdUsuario } = useUserContext();
+    const [logged] = useState(false);
+    const [cnpj, setCnpj] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleCnpjChange = (event: any) => {
+        setCnpj(event.target.value);
+    }
+
+    const handlePasswordChange = (event: any) => {
+        setPassword(event.target.value);
+    }
+
+
     return (
             <main>
 
