f0a8abe7eea1b5d87f467d27002bf6b3dd0cf910
levmn <levimncontato@gmail.com>
2024-11-22 21:41:55 -0300

    finalizando logica da navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -5,18 +5,27 @@ import { LogoIcon } from "../logoIcon/LogoIcon"
 import { FaUserCircle } from "react-icons/fa";
 import { useState } from "react";
 import { IoMenu } from "react-icons/io5";
+import { useUserContext } from "@/context/UserContext";
 
 export const Navbar = () => {
 
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const { idUsuario } = useUserContext();;
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
     const navLinks = idUsuario
-    ? [
-          { href: "/#integrantes", label: "Integrantes" },
-          { href: "/calculadora", label: "Calculadora" },
-          { href: "/registros", label: "Registros" },
-      ]
-    : [{ href: "/#integrantes", label: "Integrantes" }];
+        ? [
+              { href: "/#integrantes", label: "Integrantes" },
+              { href: "/calculadora", label: "Calculadora" },
+              { href: "/registros", label: "Registros" },
+          ]
+        : [{ href: "/#integrantes", label: "Integrantes" }];
+
     return (
-<nav className="w-full h-20 border-b-4 border-gray-200">
+        <nav className="w-full h-20 border-b-4 border-gray-200">
             <div className="w-full h-full max-w-7xfull m-auto flex flex-wrap justify-between content-center px-8 sm:px-12 lg:px-12">
                 <div className="flex">
                     <Link className="w-32 sm:w-44" href={"/"}>
