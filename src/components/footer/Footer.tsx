2b3d21fcac089f2b6080064c71f155fd070f90cd
levmn <levimncontato@gmail.com>
2024-11-22 21:42:51 -0300

    comecando footer

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,7 @@
+export const Footer = () => {
+    return (
+        <footer className="bg-black flex flex-col-reverse sm:flex-row items-center justify-between gap-8 sm:gap-0 py-10 px-8 xl:px-14">
+
+        </footer>
+    )
+}
