4fb52cb419e95b93f0c16b57a2685ec0b8741405
CaioLiang <caioliang2@gmail.com>
2024-11-22 22:08:50 -0300

    finalizando homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -76,14 +76,6 @@ export default function Home() {
           ))}
         </div>
       </section>
-      <section>
-        <div>
-          <h1>
-            Section 4
-          </h1>
-        </div>
-      </section>
-
     </main>
   )
 }
