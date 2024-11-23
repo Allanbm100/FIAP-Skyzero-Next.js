54db58dc05881b22c884c256021b1ef3609b16cb
allanbm100 <allan.bm.100@gmail.com>
2024-11-22 22:55:55 -0300

    adicionando tags de exibicao

--- src/app/registro/page.tsx
+++ src/app/registro/page.tsx
@@ -65,8 +65,35 @@ export default function Registros() {
     }, []);
 
     return (
-        <main>
-           
+        <main className="pt-10 pb-20 sm:py-20 px-8 sm:px-24 md:px-40 lg:px-60 xl:px-96">
+            <Link href={"/"} className="flex w-fit mb-12">
+                <PiKeyReturnFill size={40} color="079b11" />
+            </Link>
+            <h1 className="w-full flex flex-col items-center justify-center text-[#079b11] font-bold text-3xl sm:w-auto sm:text-4xl mb-12">Registros</h1>
+            {error && <p className="text-red-500">{error}</p>}
+
+            <div className="w-full">
+                {registros.length === 0 ? (
+                    <p className="flex justify-center text-gray-500">Nenhum registro encontrado.</p>
+                ) : (
+                    <ul className="space-y-4">
+                        {registros.map((registro, index) => (
+                            <li key={index} className="w-full flex flex-col gap-7 sm:gap-0 sm:flex-row items-center bg-gray-100 px-4 py-8 rounded-lg shadow">
+                                <div className="sm:w-1/2 flex flex-col items-center sm:items-start gap-2">
+                                    <h1><strong>Tipo de Avião:</strong></h1>
+                                    <p>{registro.tipoAviao}</p>
+                                    <h1><strong>Distância:</strong></h1>
+                                    <p>{registro.distancia} km</p>
+                                    <h1><strong>Emissão Calculada:</strong></h1>
+                                    <p>{(registro.emissaoCalculada * 1000).toFixed(2)} g de CO2</p>
+                                    <h1><strong>Data do Registro:</strong></h1>
+                                    <p>{formatDate(registro.dataRegistro)}</p>
+                                </div>
+                            </li>
+                        ))}
+                    </ul>
+                )}
+            </div>
         </main >
     )
 }
