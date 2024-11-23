cdaa62227eda1cbe67d5efaa14f0a0bbeac58a45
allanbm100 <allan.bm.100@gmail.com>
2024-11-22 22:48:58 -0300

    adicionando logica de uso

--- src/app/calculadora/page.tsx
+++ src/app/calculadora/page.tsx
@@ -1,7 +1,36 @@
+"use client"
+
+import { PiKeyReturnFill } from "react-icons/pi";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import { useUserContext } from "@/context/UserContext";
+import { useRouter } from "next/navigation";
+
 export default function Calculadora() {
+    const router = useRouter();
+
+    const [distance, setDistance] = useState(0);
+    const [valid, setValid] = useState<boolean>(false);
+    const { idUsuario } = useUserContext();
+    const [emission, setEmission] = useState<number | null>(null);
+    const [tipoAviao, setTipoAviao] = useState<"PassengerAirplane" | "CargoAirplane">("PassengerAirplane");
+
+    useEffect(() => {
+        if (!idUsuario) {
+            router.push("/login");
+        }
+    }, [idUsuario, router]);
+
+    const handleDistanceChange = (event: any) => {
+        setDistance(event.target.value)
+    }
+
+    const handleTipoAviaoChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setTipoAviao(event.target.value as "PassengerAirplane" | "CargoAirplane");
+    };
     return (
         <main>
-            
+
         </main>
     )
 }
